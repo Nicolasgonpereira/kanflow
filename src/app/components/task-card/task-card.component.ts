7b1dca6b804bbf2f0cd04485e91a5c4dc44ff329
Nicolas <nicolasgp.ec@gmail.com>
2025-01-20 20:07:20 -0300

    feat: adjust edit and delete modals for better usability

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,20 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from '../../models/task.model';
+import { BoardService } from '../../services/board/board.service';
+import { ActionConfirmationComponent } from "../action-confirmation/action-confirmation.component";
+import { EditTaskFormComponent } from "../edit-task-form/edit-task-form.component";
 import { ModalComponent } from "../modal/modal.component";
 import { TaskInfoComponent } from "../task-info/task-info.component";
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [CommonModule, ModalComponent, TaskInfoComponent],
+  imports: [CommonModule, ModalComponent, TaskInfoComponent, EditTaskFormComponent, ActionConfirmationComponent],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.css'
 })
 export class TaskCardComponent {
 	@Input() task!: Task;
+	isEditTaskModalOpen: boolean = false;
+	isDeletingModalOpen: boolean = false;
 	@Output() toggleDragActive: EventEmitter<void> = new EventEmitter;
 	isTaskInfoModalOpen: boolean = false;
+
+	constructor (private boardService: BoardService) {}
 
 	openTaskInfoModal(): void {
 		this.isTaskInfoModalOpen = true;
@@ -23,6 +30,27 @@ export class TaskCardComponent {
 
 	closeTaskInfoModal(): void {
 		this.isTaskInfoModalOpen = false;
+		this.toggleDrag();
+	}
+
+	openEditTaskModal(): void {
+		this.isEditTaskModalOpen = true;
+	}
+
+	closeEditTaskModal(): void {
+		this.isEditTaskModalOpen = false;
+	}
+
+	openDeleteModal(): void {
+		this.isDeletingModalOpen = true;
+	}
+
+	closeDeleteModal(): void {
+		this.isDeletingModalOpen = false;
+	}
+
+	deleteTask(): void {
+		this.boardService.deleteTask(this.task);
 		this.toggleDrag();
 	}
 
--- src/app/components/task-info/task-info.component.ts
+++ src/app/components/task-info/task-info.component.ts
@@ -3,24 +3,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SubTask } from '../../models/subtask.model';
 import { Task } from '../../models/task.model';
 import { BoardService } from '../../services/board/board.service';
-import { ActionConfirmationComponent } from "../action-confirmation/action-confirmation.component";
 import { DropDownComponent } from "../drop-down/drop-down.component";
-import { EditTaskFormComponent } from "../edit-task-form/edit-task-form.component";
-import { ModalComponent } from "../modal/modal.component";
 
 @Component({
   selector: 'app-task-info',
   standalone: true,
-  imports: [CommonModule, DropDownComponent, ModalComponent, EditTaskFormComponent, ActionConfirmationComponent],
+  imports: [CommonModule, DropDownComponent],
   templateUrl: './task-info.component.html',
   styleUrl: './task-info.component.css'
 })
 export class TaskInfoComponent {
 	@Input() task!: Task;
 	isDropdownSettingsOpen: boolean = false;
-	isEditTaskModalOpen: boolean = false;
-	isDeletingModalOpen: boolean = false;
 	@Output() closeModal: EventEmitter<void> = new EventEmitter();
+	@Output() openEditTaskModal: EventEmitter<void> = new EventEmitter();
+	@Output() openDeleteTaskModal: EventEmitter<void> = new EventEmitter();
 	dropDownStyles: {[key: string]: string} = {'transform': 'translate(-100%, 20px)'};
 
 	constructor (private boardService: BoardService) {}
@@ -34,28 +31,15 @@ export class TaskInfoComponent {
 		this.isDropdownSettingsOpen = false;
 	}
 
-	openEditTaskModal(): void {
-		this.isEditTaskModalOpen = true;
-	}
-
-	closeEditTaskModal(): void {
-		this.isEditTaskModalOpen = false;
+	openEditTask(): void {
+		this.openEditTaskModal.emit();
 	}
 
 	handleSubtaskCompletion(subtask: SubTask): void {
 		this.boardService.saveSubtaskCompletion(this.task, subtask);
 	}
 
-	deleteTask(): void {
-		this.boardService.deleteTask(this.task);
-		this.closeModal.emit();
-	}
-
 	openDeleteModal(): void {
-		this.isDeletingModalOpen = true;
-	}
-
-	closeDeleteModal(): void {
-		this.isDeletingModalOpen = false;
+		this.openDeleteTaskModal.emit();
 	}
 }
